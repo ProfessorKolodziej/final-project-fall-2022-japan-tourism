287c11697b461f306c80d5a92a0723d99d690299
RenxinLi-Iris <renxinli@bu.edu>
2023-12-04 00:21:04 -0500

    1

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,24 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+const submitButton = document.getElementById('submit-button');
+const feedback = document.getElementById('feedback');
+
+submitButton.addEventListener('click', checkAnswer);
+
+function checkAnswer() {
+  let selectedAnswer = document.querySelector('input[name="answer"]:checked');
+
+  if (selectedAnswer) {
+    if (selectedAnswer.value === 'C') {
+      feedback.innerHTML = "Congratulations, you answered correctly! <br>The Japanese believe that the ancestors of the crow led their one-time Emperor Shinmu out of the land of the lost, thus many Japanese worship the crow.";
+    } else {
+      feedback.innerHTML = "Unfortunately, the correct answer is C.<br> The Japanese believe that the ancestors of the crow led their one-time Emperor Shinmu out of the land of the lost, thus many Japanese worship the crow.";
+    }
+
+    feedback.style.fontFamily = 'Jost, sans-serif';
+	 feedback.style.fontSize = '13px';
+    feedback.style.color = '#df7442';
+	 feedback.style.marginTop = '10px';
+  }
+}
